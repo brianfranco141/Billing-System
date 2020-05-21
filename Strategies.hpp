27ba6610b20701e242125feb749e90de21a16f84
Brian Franco <42521586+brianfranco141@users.noreply.github.com>
2020-05-20 18:28:43 -0700

    Update Strategies.hpp

--- Strategies.hpp
+++ Strategies.hpp
@@ -36,12 +36,6 @@ using std::string;
 using std::stringstream;
 
 
-/************************************************
-** Non-Member helper function definitions
-************************************************/
-// std::stack and std::queue both provide capability to view or modify the next element to be removed.  std::stack calls is top()
-// while std::queue calls it front().  This pair of template functions provides a general concept called peek() that delegates to
-// the appropriate top() or front() for a given container.
 template<typename T>
 auto & peek( std::stack<T> & container )
 { return container.top(); }
@@ -50,11 +44,7 @@ template<typename T>
 auto & peek( std::queue<T> & container )
 { return container.front(); }
 
-/************************************************
-** Member function definitions
-************************************************/
 
-// This constructor is complete
 template<class Strategy>
 Strategy_t<Strategy>::Strategy_t( const std::string & name,
                                   double              starting,
@@ -62,7 +52,6 @@ Strategy_t<Strategy>::Strategy_t( const std::string & name,
   : myChecking_( name, starting, salary ), amount_penalties_( 0.0 )
 {}
 
-// This function is incomplete. It needs some lines of code.
 template<class Strategy>
 bool Strategy_t<Strategy>::payBills(const unsigned int & currMonth,
 
@@ -75,9 +64,6 @@ bool Strategy_t<Strategy>::payBills(const unsigned int & currMonth,
     someBill    = peek( allBills_ );
     double fee = 0.0;
     if( someBill.isOverdue( currMonth, currDay ) ) {
-      // COMPLETE BELOW:
-      // COMPUTE THE TOTAL PENALTY IN VARIABLE  <fee> AS 
-      // 35 + round(0.1 * days overdue * amount due) / 100
       fee = fee + (35 + round(0.1 * someBill.daysOverdue(currMonth, currDay) * someBill.amount_due_)/100);
     }
     
@@ -92,7 +78,6 @@ bool Strategy_t<Strategy>::payBills(const unsigned int & currMonth,
   return (allBills_.size() == 0);
 }
 
-// This function is incomplete. You need to write few lines of code.
 template<class Strategy>
 void Strategy_t<Strategy>::readFile( const std::string & filename )
 {
@@ -120,8 +105,6 @@ void Strategy_t<Strategy>::readFile( const std::string & filename )
         getline( lineStream, cell, '\n' );
         newBill.due_day_ = stoul( cell );
 
-	// COMPLETE BELOW:
-	// ADD <newBill> TO THE STACK/QUEUE OF <allBills_>
 	allBills_.push(newBill);
       }
       
@@ -138,7 +121,6 @@ void Strategy_t<Strategy>::readFile( const std::string & filename )
         getline( lineStream, cell, '\n' );
         unsigned int currDay = stoul( cell );
 	
-	// COMPLETE BELOW:
 	// CALL THE FUNCTION MEMBER TO PAY AS MANY BILLS AS POSSIBLE
 	payBills(currMonth, currDay);
 
