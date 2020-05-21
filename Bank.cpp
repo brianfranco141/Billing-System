7095f5d066dfd795816ecaa23631af33a4b6ef30
Brian Franco <42521586+brianfranco141@users.noreply.github.com>
2020-05-20 18:26:44 -0700

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -1,11 +1,5 @@
 #include <string>
-
 #include "Bank.hpp"
-
-/************************************************
-** Member function definitions
-************************************************/
-// PLEASE ADD IMPLEMENTATIONS OF ALL FUNCTION MEMBERS OF CLASS BANK
 
 Bank::Bank(const std::string &name, const double &starting, const double &salary):name_(name), amount_starting_(starting), amount_salary_(salary) {
 
